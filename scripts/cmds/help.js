5bce7024d1b1da833c3fd4483605c010c562e2f4
Rucoyas <yasisarmia@gmail.com>
2025-01-09 22:28:52 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🦋 ArYAN 🌺";
+const doNotDelete = "Chesca Ai";
 /** 
 * @author NTKhang
 * @author: do not delete it
