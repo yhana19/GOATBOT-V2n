f42296d20b9643cf4b17f2cbd4f1f2a41ce9a275
Yhana alvarez <yasisarmia@gmail.com>
2025-01-24 01:32:08 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "🎀𝗖𝗛𝗘𝗦𝗖𝗔 𝗔𝗜 𝗩2🎀";
+const doNotDelete = "🌏CHERA WORLD 𝗩2";
 /** 
 * @author NTKhang
 * @author: do not delete it
